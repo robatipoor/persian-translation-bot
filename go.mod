542a987a1bfb1494d3d2755aea62ac62d05c21fa
robatioor <mahdi.robatipoor@gmail.com>
2019-02-26 21:29:53 +0330

    add scheduling task

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/robatipoor/persian-translation-bot
 require (
 	github.com/abadojack/whatlanggo v0.0.0-20181004111500-01d4c01a5a7c
 	github.com/evalphobia/google-tts-go v0.3.0
+	github.com/jasonlvhit/gocron v0.0.0-20190121134850-6771d4b492ba
 	github.com/pkg/errors v0.8.1 // indirect
 	github.com/tidwall/gjson v1.1.5
 	github.com/tidwall/match v1.0.1 // indirect
--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 
-var addr string
+var address string
 
 func init() {
 
@@ -18,14 +18,16 @@ func init() {
 	if os.Getenv("TELEGRAM_TOKEN") == "" {
 		log.Fatalln("TELEGRAM_TOKEN not set !")
 	}
-	addr = "0.0.0.0:" + port
+	address = "0.0.0.0:" + port
 }
 
 func main() {
 	go StartBot()
+	// for live in heroku cloud
+	go scheduledTask()
 	http.HandleFunc("/", handler)
-	log.Printf("Start Server %s ...\n", addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
+	log.Println("Start Server : ", address)
+	log.Fatal(http.ListenAndServe(address, nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
