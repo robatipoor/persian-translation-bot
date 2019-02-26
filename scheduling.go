926cb33a75b241990c1707e2c3b123e892013661
robatioor <mahdi.robatipoor@gmail.com>
2019-02-26 22:05:40 +0330

    add scheduling task

--- scheduling.go
+++ scheduling.go
@@ -10,11 +10,11 @@ func scheduledTask() {
 
 	s := gocron.NewScheduler()
 	s.Every(30).Minutes().Do(func() {
-		b, err := get("https://persiantranslationbot.herokuapp.com/")
+		s, err := get("https://persiantranslationbot.herokuapp.com/")
 		if err != nil {
 			log.Println(err)
 		}
-		log.Println(string(b))
+		log.Println(s)
 	})
 	log.Println("Start Scheduled Task")
 	<-s.Start()
