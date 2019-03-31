90ae19d6eaf20c623e3de2ca4018472f6ef74579
mahdi <mahdi.robatipoor@gmail.com>
2019-03-31 19:32:37 +0430

    support webhook

--- main.go
+++ main.go
@@ -1,33 +1,81 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"log"
-	"net/http"
 	"os"
+
+	tb "gopkg.in/tucnak/telebot.v2"
 )
 
-var address string
+var port string
+var token string
+var appURL string
 
 func init() {
-	port := os.Getenv("PORT")
+	port = os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-	if os.Getenv("TELEGRAM_TOKEN") == "" {
+	appURL = os.Getenv("APP_URL")
+	if appURL == "" {
+		log.Fatal("Application URL not set")
+	}
+	token = os.Getenv("TELEGRAM_TOKEN")
+	if token == "" {
 		log.Fatalln("TELEGRAM_TOKEN not set !")
 	}
-	address = "0.0.0.0:" + port
 }
 
 func main() {
-	go StartBot()
-	http.HandleFunc("/", handler)
-	log.Println("Start Server : ", address)
-	log.Fatal(http.ListenAndServe(address, nil))
-}
+	
+	webhook := &tb.Webhook{
+		Listen:   ":" + port,
+		Endpoint: &tb.WebhookEndpoint{PublicURL: appURL},
+	}
+	setings := tb.Settings{
+		Token:  token,
+		Poller: webhook,
+	}
+	bot, err := tb.NewBot(setings)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bot.Handle(tb.OnText, func(m *tb.Message) {
+		if m.Text == "/start" {
+			log.Println("show help message")
+			bot.Send(m.Sender, "Please type in English or Farsi and send me !")
+			return
+		}
+		targetLang, err := TargetLang(m.Text)
+		if err != nil {
+			log.Println(err)
+			bot.Send(m.Sender, "Detect Language Faild !")
+			return
+		}
+		tr, err := Translate(m.Text, string(targetLang))
+		if err != nil {
+			log.Println(err)
+			bot.Send(m.Sender, "Translate Problem !")
+			return
+		}
+		log.Println(tr)
+		if targetLang == FA {
+			speech, err := TextToSpeech(m.Text)
+			if err != nil {
+				log.Println(err)
+				bot.Send(m.Sender, "Text To Speech Problem !")
+				return
+			}
+			audio := &tb.Audio{File: tb.FromReader(bytes.NewBuffer(speech))}
+			audio.Title = m.Text
+			audio.Caption = tr
+			bot.Send(m.Sender, audio)
+		} else {
+			bot.Send(m.Sender, tr)
+		}
+	})
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, "Start Bot !")
+	log.Println("Start Bot ...")
+	bot.Start()
 }
